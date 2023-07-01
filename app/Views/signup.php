6bf4f83c315a1b4a28df5944ff6d4b73b45993ce
sourabhvatscoder <sourabhvats96@gmail.com>
2023-07-01 12:54:37 +0530

    test

--- app/Views/signup.php
+++ app/Views/signup.php
@@ -1,18 +1,3 @@
-<?php
-// $user_type = "";
-// if (empty($_GET["plan"])) {
-//     header("LOCATION: /");
-//     die();
-// } else {
-//     $user_type = $_GET["plan"];
-//     $booking_packages_number = 1;
-//     if ($user_type == "micro") {
-//         $booking_packages_number = $_GET["micro"];
-//     }else if ($user_type == "macro") {
-//         $booking_packages_number = 5;
-//     }
-// }
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="Travmax">
     <meta name="description" content=" We offer flights, packages, hotels, corporate travel services, the best customized travel support and more.">
+    <meta property="og:title" content="RealSatisfied"/>
     <meta property="og:image" content="http://travmaxholidays.com/images/logo-social.jpg"/>
     <title>Signup</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
