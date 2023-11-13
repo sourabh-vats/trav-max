7b5ef9a795df636a8102dad28195b72e3735ebd3
sourabhvatscoder <sourabhvats96@gmail.com>
2023-11-13 11:16:30 +0530

    fix

--- app/Views/confirm_plan.php
+++ app/Views/confirm_plan.php
@@ -63,15 +63,15 @@ switch ($plan) {
                     <span>You've selected</span><br>
                     <span class="heading_1"><?php echo $package["display_name"]; ?> package</span><br>
                     <span>for</span>
-                    <span><?php echo $package["nights"] . " " . $package["days"]; ?></span>
+                    <span><?php echo $package["nights"] ." ". $package["days"];?></span>
                     <br>
                     <br>
                     <span>The Partnership you have selected is<br><span class="heading_1"><?php echo ucfirst($plan); ?></span></span>
                     <br>
                     <br>
                     <span>You need to make a payment of <br><span class="heading_1">Rs <?php echo $payment_amount; ?></span></span>
-                <p class="h4">Plus Taxes</p>
-                <p class="h5">TCS as applicable.</p>
+                    <p class="h4">Plus Taxes</p>
+                    <p class="h5">TCS as applicable.</p>
                 </p>
                 <br>
             </div>
@@ -79,27 +79,28 @@ switch ($plan) {
         <?php
         helper('form');
         $user = $profile[0];
+        $attributes = array('class' => 'form');
+        echo form_open_multipart(base_url('signup/confirm_plan'), $attributes);
         ?>
-        <form action="/signup/confirm_plan" method="post">
-            <input type="hidden" name="package_id" value="<?= $package['id']; ?>">
-            <input type="hidden" name="plan" value="<?php echo $_GET["plan"]; ?>">
-            <input type="hidden" name="payment_plan" value="<?php echo $_GET["payment_plan"]; ?>">
-            <div class="d-flex justify-content-center" style="padding-top: 30px;">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                    <label class="form-check-label" for="invalidCheck">
-                        <strong> I Agree to <a href="/terms_of_use" target="_blank">terms and conditions</a></strong>
-                    </label>
-                    <div class="invalid-feedback">
-                        You must agree before submitting.
-                    </div>
+        <input type="hidden" name="package_id" value="<?= $package['id']; ?>">
+        <input type="hidden" name="plan" value="<?php echo $_GET["plan"]; ?>">
+        <input type="hidden" name="payment_plan" value="<?php echo $_GET["payment_plan"]; ?>">
+        <div class="d-flex justify-content-center" style="padding-top: 30px;">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                <label class="form-check-label" for="invalidCheck">
+                    <strong> I Agree to <a href="/terms_of_use" target="_blank">terms and conditions</a></strong>
+                </label>
+                <div class="invalid-feedback">
+                    You must agree before submitting.
                 </div>
             </div>
-            <div class="text-center">
-                <button class="primary_btn my-3" id="book_package_btn" type="submit">Confirm</button>
-                <a href="/signup/choose_payment_plan?plan=<?php echo $_GET["plan"]; ?>&package=<?php echo $_GET["package"]; ?>" class="primary_btn">Change Plan</a>
-            </div>
-        </form>
+        </div>
+        <div class="text-center">
+            <button class="primary_btn my-3" id="book_package_btn" type="submit">Confirm</button>
+            <a href="/signup/choose_payment_plan?plan=<?php echo $_GET["plan"]; ?>&package=<?php echo $_GET["package"]; ?>" class="primary_btn">Change Plan</a>
+        </div>
+        <?= form_close(); ?>
     </div>
     <script>
 
