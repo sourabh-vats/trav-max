136c9fdf8b1d38c36633726c721a7b778a991289
sourabhvatscoder <sourabhvats96@gmail.com>
2024-01-23 16:37:03 +0530

    refer ids updated

--- app/Views/signup.php
+++ app/Views/signup.php
@@ -244,7 +244,7 @@
             });
             $("#refergentick").click(function() {
                 if ($("#refergentick").is(":checked")) {
-                    const freeRefferalIDs = ["1085MEM3665", "1086MEM3665", "1087MEM3665", "1088MEM3665", "1089MEM3665", "1090MEM3665", "1091MEM3665", "1092MEM3665", "1093MEM3665", "1094MEM3665", "1095MEM3665", "1096MEM3665", "1097MEM3665", "1098MEM3665", "1098MEM3665", "1099MEM3665", "1100MEM3665", "1101MEM3665", "1102MEM3665", "1103MEM3665", "1104MEM3665", "1105MEM3665", "1106MEM3665", "1107MEM3665", "1108MEM3665", "1108MEM3665"];
+                    const freeRefferalIDs = ["1154MEM0011", "1155MEM0012", "1156MEM0013", "1157MEM0014", "1158MEM0015"];
                     const random = Math.floor(Math.random() * freeRefferalIDs.length);
                     $("#trav_id").val(freeRefferalIDs[random]);
                 } else {
